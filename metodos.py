35efd75610faf33e50feff3a058d26c30a62c595
LauraToledoGutierrez <Laura.Toledo3@alu.uclm.es>
2021-11-27 22:05:59 +0100

    cambios metodos

--- metodos.py
+++ metodos.py
@@ -2,9 +2,28 @@ import sys
 import uuid
 import json
 
+def getTile(self, id):
+    data = json.loads(open('infoPeliculas.json').read())
+    for ids in data["idP"]:
+        if(ids["idP"]==id):
+             tile = data.get("nombre")
+    print("El titulo de la pelicula es: %s", tile)
+    return tile
+
+#no es asi jj
+def getTilesByName(self, name, exact ):
+    data = json.loads(open('infoPeliculas.json').read())
+    for nombres in data["nombre"]:
+        if(nombres["nombre"]==name):
+            tile = data.get("nombre")
+    return tile
+
+
+
 def addTags(self, id, tag, userToken):
      data = json.loads(open('catalogueMedia.json').read())
      for tag in data["tags"]:
+         
 
 
 def removeTags(id, nameTag, adminToken, current=None):

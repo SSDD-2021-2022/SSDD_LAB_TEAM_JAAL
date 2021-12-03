6fc8989d127c91994fd844871cd225367120e041
AntonioPtnR <Antonio.Paton2@alu.uclm.es>
2021-12-03 20:45:25 +0100

    Main mejorado para que se comunique con el authenticator(distribuido)

--- Main.py
+++ Main.py
@@ -5,6 +5,9 @@ import Ice
 Ice.loadSlice('iceflix.ice')
 import IceFlix
 
+listaObjAuth = []
+listaObjCatg = []
+
 class MainI(IceFlix.Main):
     def isAdmin(self, adminToken, current = None):
         admin = False
@@ -14,11 +17,42 @@ class MainI(IceFlix.Main):
         
         return admin
 
+    def register(self, service, current = None):
+        try:
+            #Se comprueba el tipo de objeto remoto que llega
+            print(service.ice_id())
+            if(service.ice_isA("::IceFlix::Authenticator")):
+                listaObjAuth.append(service)
+            if(service.ice_isA("::IceFlix::MediaCatalog")):
+                listaObjCatg.append(service)
+            print(listaObjAuth)
+        except IceFlix.UnknownService:
+            print("Servicio desconocido")
+
+    def getAuthenticator(self, current=None):
+        try:
+            if(len(listaObjAuth) !=0 and listaObjAuth[0].ice_ping()):
+                print("holaaaaaaaaaaa")
+                return listaObjAuth[0]
+            else:
+                raise IceFlix.TemporaryUnavailable
+        except IceFlix.TemporaryUnavailable:
+            print("No hay ningun servicio de autenticacion abierto")
+    
+    def getCatalog(self, current=None):
+        try:
+            if(len(listaObjCatg) !=0 and listaObjCatg[0].ice_ping()):
+                return listaObjCatg[0]
+            else:
+                raise IceFlix.TemporaryUnavailable
+        except IceFlix.TemporaryUnavailable:
+            print("No hay ningun servicio de catalogo abierto")
+
 class ServerMain(Ice.Application):
     def run(self, args):
         broker = self.communicator()
         servant = MainI()
-        #print(aux.isAdmin("blasss"))
+        servant.getAuthenticator()
         adapter = broker.createObjectAdapter("MainAdapter")
         proxy = adapter.add(servant, broker.stringToIdentity("main1"))
 
